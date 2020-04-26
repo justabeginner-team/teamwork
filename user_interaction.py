ad757df59938ca84782bb1ace022f80f39aa10cd
IanMaguithi <maguithian13427@gmail.com>
2020-04-26 11:30:25 +0300

    added a like by feed action

--- user_interaction.py
+++ user_interaction.py
@@ -140,6 +140,8 @@ def liking_posts(hashtags):
     # with banned and spammy tags filtered out
     session.set_smart_hashtags(hashtags, limit=5, sort='top', log_tags=True)
     session.like_by_tags(amount=10, use_smart_hashtags=True)
+    # liking posts on own feeds
+    session.like_by_feed(amount=90, randomize=True, unfollow=True, interact=True)
 
 
 def engagement_pods():
